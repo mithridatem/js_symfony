f680ba7fe3663796cc5167342e286bcf12c17901
mithridatem <72612782+mithridatem@users.noreply.github.com>
2023-02-15 03:23:57 +0100

    add vue

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,15 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TaskController extends AbstractController
 {
     #[Route('/task', name: 'app_task')]
-    public function showAllTask(): Response
+    public function showAllTask(TaskRepository $repo): Response
     {
-        return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+        //récupération de la liste des tâches dont status = 1
+        $tasks = $repo->findBy(['status' => 1]);
+        //rendu du template twig
+        return $this->render('task/index.html.twig',[
+                'tasks' => $tasks, 
         ]);
     }
 }
