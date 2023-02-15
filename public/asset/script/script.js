6da2938c1fdcb28828971559c12193a5dd2f9d96
mithridatem <72612782+mithridatem@users.noreply.github.com>
2023-02-15 04:27:50 +0100

    add methode update controller

--- public/asset/script/script.js
+++ public/asset/script/script.js
@@ -3,12 +3,32 @@ console.log(tasks);
 
 tasks.forEach(element => {
     element.children[2].addEventListener('click', (e)=>{
-        const json = {'id':element.children[1].id, 'name':element.children[0].textContent,'status':true}
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", '/task/update', true);
-        //Envoie les informations du header adaptées avec la requête
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(json); 
-        console.log(json);})
+        //test si checked
+        if(element.children[1].checked){
+             //création du json
+            const json = {'id':element.children[1].id, 'name':element.children[0].textContent,'status':true}
+            let xhr = new XMLHttpRequest();
+            //connexion
+            xhr.open("POST", '/task/update/'+element.children[1].id, true);
+            //Envoie les informations du header adaptées avec la requête
+            xhr.setRequestHeader("Content-Type", "application/json");
+            //Appelle une fonction au changement d'état.
+            xhr.onreadystatechange = function() { 
+                if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                    //refresh ton interface
+                    console.log('ok');
+                }else{
+                    console.log('erreur');
+                }
+            }
+            //envoi du json
+            xhr.send(json);
+            //suppression de l'element
+            element.remove();
+            console.log(json);
+        }else{
+            console.log('cocher la checkbox');
+        }
+       })
 });
     
--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -9,6 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Task;
 use App\Repository\TaskRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class TaskController extends AbstractController
 {
@@ -21,5 +23,47 @@ class TaskController extends AbstractController
         return $this->render('task/index.html.twig',[
                 'tasks' => $tasks, 
         ]);
+    }
+    //fonction qui ajoute une nouvelle catégorie depuis un json
+    #[Route('/task/update/{id}', name: 'app_task_update', methods: 'POST')]
+    public function updatetask(taskRepository $repo,
+    EntityManagerInterface $manager, Request $request,
+    SerializerInterface $serializer, $id
+    ): Response
+    {
+        //récupération de l'objet si il existe
+        $task = $repo->find($id);
+        //test si la catégorie existe
+        if($task == null){
+            return $this->json(['error'=>'La Tache n\'existe pas'],200,
+            ['Content-Type'=>'application/json','Access-Control-Allow-Origin'=> '*',
+            'Access-Control-Allow-Methods'=>'PATCH']);
+        }
+        //test si elle existe
+        else{
+            //récupérer le json avec Request
+            $json = $request->getContent();
+            //tester si on à bien récupéré un json
+            if($json != null){
+                //décoder le json -> convertir en tableau
+                $recup = $serializer->decode($json, 'json');
+                //setter le nouveau status
+                $task->setStatus($recup['status']);
+                //sauvegarder la modification dans le manager
+                $manager->persist($task);
+                //envoyer la modification a la BDD
+                $manager->flush();
+                //retourne un json
+                return $this->json(['error'=>'Le status à été modifié'],200,
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin'=> '*']);
+                
+            }
+            //test si je n'ai pas de json dans le résultat de la requête
+            else{
+                //retourne une erreur pas de json
+                return $this->json(['error'=>'L\'entête http ne contient aucun json'],200,
+                ['Content-Type'=>'application/json','Access-Control-Allow-Origin'=> '*']);
+            }
+        }
     }
 }
